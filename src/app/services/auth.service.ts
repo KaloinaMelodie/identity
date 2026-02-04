79f24e0f4f73dfa08e5221c1a5a3c257a7edbf92
RAVOAHANGILALAO Anjaniaina Kaloina Melodie <99157194+KaloinaMelodie@users.noreply.github.com>
2026-02-04 22:55:25 +0300

    Update auth.service.ts

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable, tap } from 'rxjs';
 import { Router } from '@angular/router';
+import { environment } from '../../environments/environment';
 
 export interface LoginResponse {
   name: string;
@@ -13,7 +14,7 @@ export interface LoginResponse {
   providedIn: 'root'
 })
 export class AuthService {
-  private apiUrl = 'http://localhost:8000/api'; 
+  private apiUrl = environment.apiUrl; 
   private currentUserSubject = new BehaviorSubject<LoginResponse | null>(this.getUserFromStorage());
   currentUser$ = this.currentUserSubject.asObservable();
 
